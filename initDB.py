383fc776ba10a90847048c451920b20d14f98019
Boldoosang <62372461+Boldoosang@users.noreply.github.com>
2021-04-11 07:29:31 -0400

    Model relationships updated

--- initDB.py
+++ initDB.py
@@ -1,10 +1,36 @@
 from main import app
-from models import db, Club
+from models import db, Club, Election, User, Candidate
+import os
+
+os.remove("test.db")
 
 db.create_all(app=app)
 
 UWICS = Club(clubName="UWICS")
-db.session.add(compSciClub)
+db.session.add(UWICS)
 db.session.commit()
+
+user1 = User("bob", "bobpass", "Bob", "Johnson")
+user2 = User("tom", "tompass", "Tom", "Johnson")
+
+db.session.add(user1)
+db.session.add(user2)
+db.session.commit()
+
+user1.joinClub("UWICS")
+
+election1 = Election(position="Secretary", hostingClub=UWICS)
+election2 = Election(position="President", hostingClub=UWICS)
+db.session.add(election1)
+db.session.add(election2)
+db.session.commit()
+
+candidate1 = Candidate(electionID=1, firstName="Tim", lastName="Rolled")
+db.session.add(candidate1)
+db.session.commit()
+
+user1.castVote("UWICS", 1)
+user1.castVote("UWICS", 1)
+
 
 print('database initialized!')
--- models.py
+++ models.py
@@ -4,35 +4,72 @@ db = SQLAlchemy()
 import datetime
 
 class User(db.Model):
-    username = db.Column(db.String(25), primary_key=True)
+    userID = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True)
     password = db.Column(db.String(256), nullable=False)
     firstName = db.Column(db.String(50), nullable=False)
     lastName = db.Column(db.String(50), nullable=False)
+    clubMembers = db.relationship('ClubMember', backref='membership')
 
     def __init__(self, username, password, firstName, lastName):
         self.username = username
         self.firstName = firstName
         self.lastName = lastName
-        generate_password_hash(password, method='sha256')
+        self.setPassword(password)
 
     def toDict(self):
         return {
+            "userID" : self.userID,
             "username" : self.username,
             "password" : self.password,
             "firstName" : self.firstName,
             "lastName" : self.lastName
         }
 
-    def set_password(self, password):
+    def setPassword(self, password):
         self.password = generate_password_hash(password, method='sha256')
     
-    def check_password(self, password):
+    def checkPassword(self, password):
         return check_password_hash(self.password, password)
+
+    def joinClub(self, clubName):
+        result = db.session.query(Club).filter_by(clubName=clubName).first()
+
+        if not result:
+            print("User attempted to join a club that did not exist!")
+            return False
+        
+        try:
+            foundClubID = result.clubID 
+            newClubMember = ClubMember(clubID = foundClubID, userID = self.userID)
+
+            db.session.add(newClubMember)
+            db.session.commit()
+            print("User added to club!")
+            return True
+        except:
+            db.session.rollback()
+            print("Unable to add user to club!")
+
+        return False
+    
+    def castVote(self, clubName, candidateID):
+        electionClub = db.session.query(Club).filter_by(clubName=clubName).first()
+        clubMembership = db.session.query(ClubMember).filter_by(clubID=electionClub.clubID, userID=self.userID).first()
+        
+        if not clubMembership:
+            print("User is not a member of this club!")
+            return False
+        
+        return clubMembership.castVote(candidateID)
+
+
 
 class Club(db.Model):
     clubID = db.Column(db.Integer, primary_key=True)
     clubName = db.Column(db.String(80), nullable=False)
-    elections = db.relationship('Election', backref='Club')
+    elections = db.relationship('Election', backref='hostingClub')
+    members = db.relationship('ClubMember', backref='club')
 
     def toDict(self):
         return {
@@ -46,6 +83,7 @@ class Election(db.Model):
     position = db.Column(db.String(50), nullable=False)
     #electionEndDate = db.Column(db.DateTime, nullable=False, default="")
     #electionWinner = db.String(db.String(150), nullable=False)
+    candidates = db.relationship('Candidate', backref='candidate')
 
     def toDict(self):
         return {
@@ -62,12 +100,13 @@ class Election(db.Model):
     def declareWinner(self):
         pass
 
-
 class Candidate(db.Model):
     candidateID = db.Column(db.Integer, primary_key=True)
-    electionID = db.Column(db.Integer, nullable=False)
+    electionID = db.Column(db.Integer, db.ForeignKey('election.electionID'), nullable=False)
     firstName = db.Column(db.String(50), nullable=False)
     lastName = db.Column(db.String(50), nullable=False)
+    votes = db.relationship('ElectionBallot', backref='ballot')
+
 
     def toDict(self):
         return {
@@ -77,18 +116,42 @@ class Candidate(db.Model):
             "lastName" : self.lastName
         }
 
-class ElectionBallot(db.Model):
-    ballotID = db.Column(db.Integer, primary_key=True)
-    memberID = db.Column(db.Integer, nullable=False)
-    candidateID = db.Column(db.Integer, nullable=False)
-
 class ClubMember(db.Model):
     memberID = db.Column(db.Integer, primary_key=True)
-
+    clubID = db.Column(db.Integer, db.ForeignKey("club.clubID"), nullable=False)
+    userID = db.Column(db.Integer, db.ForeignKey("user.userID"), nullable=False)
+    votesMade = db.relationship('ElectionBallot', backref='voter')
+    
     def toDict(self):
-        return{
-            'id': self.id,
-            'studentId': self.studentId,
-            'stream': self.stream,
-            'created': self.created.strftime("%m/%d/%Y, %H:%M:%S")
+        return {
+            'memberID': self.memberID,
+            #complete
         }
+    
+    def castVote(self, candidateID):
+        #Do check to ensure that the election end date has not passed.
+        candidate = db.session.query(Candidate).filter_by(candidateID=candidateID).first()
+
+        if not candidate:
+            print("User attempted to join a club that did not exist!")
+            return False
+        
+        try:
+            ballot = ElectionBallot(memberID=self.memberID, candidateID=candidateID)
+            db.session.add(ballot)
+            db.session.commit()
+            return True
+        except:
+            db.session.rollback()
+            print("User may have already voted!")
+            return False
+
+
+    def callElection(self, clubName):
+        pass
+
+
+class ElectionBallot(db.Model):
+    #ballotID = db.Column(db.Integer, primary_key=True)
+    memberID = db.Column(db.Integer, db.ForeignKey("club_member.memberID"), primary_key=True)
+    candidateID = db.Column(db.Integer, db.ForeignKey("candidate.candidateID"), primary_key=True)
