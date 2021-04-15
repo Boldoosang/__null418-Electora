1094d3057dfba5700df74f125a8859ffd0c41514
Boldoosang <62372461+Boldoosang@users.noreply.github.com>
2021-04-15 06:00:32 -0400

    Update

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ from models import db, Club, Election, User, ClubMember, Candidate, ElectionBall
 def create_app():
   app = Flask(__name__, static_url_path='')
   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-  #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\jbald\Desktop\\__null418-ElectoraV2\\test.db'
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
   app.config['SECRET_KEY'] = "MYSECRET"
   app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7)
